b32b06a197df5bca8c12185d95a0a4807d0bcd5d
zhangzhenhua <2427218738@qq.com>
2022-01-12 17:51:42 +0800

    添加工具类  array.Arrays   --判断int[]是否有虚

---
+++ Tools/src/array/Arrays.java
@@ -0,0 +1,17 @@
+package array;
+
+public class Arrays {
+    /**
+     * 检测数组是否有序,无序返回false
+     */
+    private Arrays(){}
+
+    /**
+     * 检测int数组是否有序,(升 ,降 不变) true
+     * @param arr
+     * @return
+     */
+    public static boolean isOrderly(int[] arr){
+        return true;
+    }
+}
--- Tools/src/speedTest/GitTest.java
+++ Tools/src/speedTest/GitTest.java
@@ -1,4 +1,5 @@
 package speedTest;
 
 public class GitTest {
+
 }
